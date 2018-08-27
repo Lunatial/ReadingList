9d3bb86a285561027de200d506997f48ab72f940
Feleky Mirkó <feleky@gmail.com>
2018-08-27 14:36:31 +0200

    add alphabetic order

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -9,7 +9,8 @@ class BookList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: null
+            selected: null,
+            alphabeticOrder: "a-z"
         }
     }
 
@@ -26,25 +27,55 @@ class BookList extends Component {
         return comparison;
     };
 
+    compareBack = (a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+
+        let comparison = 0;
+        if (nameA > nameB) {
+            comparison = -1;
+        } else if (nameA < nameB) {
+            comparison = 1;
+        }
+        return comparison;
+    };
+
     displayBooks() {
         const data = this.props.data;
         if (data.loading) {
             return (<div>Loading books...</div>);
         } else {
-            let myObject = Object.assign([], data.books);
-            myObject.sort(this.compare);
-            return myObject.map(book => {
-                return (
-                    <li key={book.id}
-                        onClick={() => this.setState({selected: book.id})}
-                        style={{
-                            backgroundColor: this.state.selected === book.id ? "#B0E0E6" : ""
-                        }}
-                    >
-                        {book.name}
-                    </li>
-                );
-            })
+            if (this.state.alphabeticOrder === "a-z") {
+                let myObject = Object.assign([], data.books);
+                myObject.sort(this.compare);
+                return myObject.map(book => {
+                    return (
+                        <li key={book.id}
+                            onClick={() => this.setState({selected: book.id})}
+                            style={{
+                                backgroundColor: this.state.selected === book.id ? "#B0E0E6" : ""
+                            }}
+                        >
+                            {book.name}
+                        </li>
+                    );
+                })
+            } else {
+                let myObject = Object.assign([], data.books);
+                myObject.sort(this.compareBack);
+                return myObject.map(book => {
+                    return (
+                        <li key={book.id}
+                            onClick={() => this.setState({selected: book.id})}
+                            style={{
+                                backgroundColor: this.state.selected === book.id ? "#B0E0E6" : ""
+                            }}
+                        >
+                            {book.name}
+                        </li>
+                    );
+                })
+            }
         }
     }
 
@@ -53,6 +84,8 @@ class BookList extends Component {
             && this.props.data.books.findIndex(book => book.id === this.state.selected) !== -1;
         return (
             <div>
+                <button id="a-z" onClick={() => this.setState({alphabeticOrder: "a-z"})}>A->Z</button>
+                <button id="z-a" onClick={() => this.setState({alphabeticOrder: "z-a"})}>Z->A</button>
                 <ul id="book-list">
                     {this.displayBooks()}
                 </ul>
